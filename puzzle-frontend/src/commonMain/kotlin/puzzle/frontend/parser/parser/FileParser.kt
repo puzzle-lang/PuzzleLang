26e65f8cdd0422a807284f3fc39a467287601c64
李佳伟 <2965502228@qq.com>
2026-02-16 01:34:22 +0800

    feat: 支持识别其他文件的标识符

--- puzzle-frontend/src/commonMain/kotlin/puzzle/frontend/parser/parser/FileParser.kt
+++ puzzle-frontend/src/commonMain/kotlin/puzzle/frontend/parser/parser/FileParser.kt
@@ -10,19 +10,19 @@ import puzzle.token.kinds.NamespaceKind.IMPORT
 
 context(file: FileContext, cursor: PzlTokenCursor)
 fun parseFile(): AstFile {
-	val start = cursor.current.location
-	val packageDirective = parsePackageDirective()
-	val importDirectives = mutableListOf<ImportDirective>()
-	while (cursor.match(IMPORT)) {
-		importDirectives += parseImportDirective()
-	}
-	val declarations = parseDeclarations()
-	val end = cursor.previous.location
-	return AstFile(
-		builtin = false,
-		packageDirective = packageDirective,
-		importDirectives = importDirectives,
-		declarations = declarations,
-		location = start span end
-	)
+    val start = cursor.current.location
+    val packageDirective = parsePackageDirective()
+    val importDirectives = mutableListOf<ImportDirective>()
+    while (cursor.match(IMPORT)) {
+        importDirectives += parseImportDirective()
+    }
+    val declarations = parseDeclarations()
+    val end = cursor.previous.location
+    return AstFile(
+        builtin = false,
+        packageDirective = packageDirective,
+        importDirectives = importDirectives,
+        declarations = declarations,
+        location = start span end
+    )
 }
